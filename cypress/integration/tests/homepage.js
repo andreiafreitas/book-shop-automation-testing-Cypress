95c51c5667ec1ba3721382b3bed62d7e8d3a1f41
Andreia Freitas <andgofreitas@gmail.com>
2021-04-15 15:56:20 +0100

    subscribe test with email

--- cypress/integration/tests/homepage.js
+++ cypress/integration/tests/homepage.js
@@ -1,6 +1,16 @@
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 10 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(10);
+    });
+}
+
+beforeEach(() => {
+    cy.visit('http://practice.automationtesting.in/')
+})
+
 describe('Homepage', () => {
     it('As a Guest I want to see on the Homepage a highlight slider', () => {
-        cy.visit('http://practice.automationtesting.in/')
         cy.get('[id=n2-ss-6-arrow-next]').click({ multiple: true, force: true})
         cy.wait(2000)
         cy.get('[id=n2-ss-6-arrow-next]').click({ multiple: true, force: true})
@@ -9,7 +19,6 @@ describe('Homepage', () => {
     })
 
     it('As a Guest I want to see the latest arrivals on the Homepage', () => {
-        cy.visit('http://practice.automationtesting.in/')
         cy.get('[class="themify_builder_sub_row clearfix gutter-default   sub_row_1-0-2"]').children().should('have.length', 3)
     })
 
@@ -18,19 +27,18 @@ describe('Homepage', () => {
     //   cy.click('[id="menu-icon"]').get('[id=s]').scrollIntoView().type('{movetostart}HTML')
     //})
 
-    it.('As a Guest I want to check social media connections', () => {
-        cy.visit('http://practice.automationtesting.in/')
-
-        cy
-            .get('[title="Facebook"]')
+    it('As a Guest I want to check social media connections', () => {
+        cy.get('[title="Facebook"]')
             .should('have.attr', 'href', 'https://www.facebook.com/automationtesting2016/')
             .should('have.attr', 'target', '_blank')
 
             // .invoke('removeAttr', 'target').click(); testing just one button
-
     })
 
-    it.only('As a Guest I want to subscribe the page to receive notifications about the website', () => {
-
+    it('As a Guest I want to subscribe the page to receive notifications about the website', () => {
+        cy.get('[placeholder="Your email address"]').type(uuidv4()+'test@test.io')
+        cy.get('[value="Subscribe"]').click()
+        cy.get('[class="mc4wp-alert mc4wp-success"]')
     })
+
 })
